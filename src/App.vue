195f76df501044314cb024f0cb766f2cd0897da9
someone-s <workemaileden@gmail.com>
2025-08-05 11:13:11 +0100

    functioning logic

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,130 @@ import ConvertPanel from './components/ConvertPanel.vue';
 import DownloadPanel from './components/DownloadPanel.vue';
 import SettingsPanel from './components/SettingsPanel.vue';
 import UploadPanel from './components/UploadPanel.vue';
-
 import { FFmpeg, type LogEvent } from '@ffmpeg/ffmpeg';
+import { mimeLookup } from './format';
+import { downloadZip } from 'client-zip';
 
 const baseUrl = 'https://unpkg.com/@ffmpeg/core@0.12.10/dist/esm'
 const ffmpeg = new FFmpeg();
 
 let storedFiles = [] as File[];
+let storedFormat = '';
 
 async function setFile(files: File[]) {
   storedFiles = files;
 }
 
+async function setFormat(format: string) {
+  storedFormat = format;
+}
+
 async function execute() {
-  console.log('0');
+  if (!(storedFiles.length > 0 && storedFormat in mimeLookup))
+    return;
+
   ffmpeg.on('log', ({ message: msg }: LogEvent) => {
-        console.log(msg)
-      })
+    console.log(msg)
+  })
   await ffmpeg.load({
     coreURL: await toBlobURL(`${baseUrl}/ffmpeg-core.js`, 'text/javascript'),
     wasmURL: await toBlobURL(`${baseUrl}/ffmpeg-core.wasm`, 'application/wasm'),
   });
-  
+
   storedFiles.forEach(async (file) => {
-    await ffmpeg.writeFile(file.name, await fetchFile(file));
-    await ffmpeg.exec(['-i', file.name, 'test.mp4']);
-    const data = await ffmpeg.readFile('test.mp4');
-    const blob = new Blob([(data as Uint8Array).buffer], { type: 'video/mp4' });
-    const url = URL.createObjectURL(blob);
+    const tempDir = 'tempDir';
 
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = "test.mp4";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    const segement = file.name.split('.');
+    if (segement.pop() === storedFormat)
+      return;
 
-    URL.revokeObjectURL(url);
+    const splitMode: boolean = mimeLookup[storedFormat].startsWith('image');
+    let archiveName: string = '';
+
+    if (splitMode) {
+      const archiveSegment = segement.slice();
+      archiveSegment.push('zip');
+      archiveName = archiveSegment.join('.');
+
+      if (segement.length == 0)
+        segement.unshift('');
+      segement[segement.length - 1] = segement[segement.length - 1] + '%05d';
+    }
+    segement.push(storedFormat);
+    const outputName = segement.join('.');
+
+    const inputPath = `${tempDir}/${file.name}`;
+    const outputPath = `${tempDir}/${outputName}`;
+
+    await ffmpeg.createDir(tempDir);
+
+    await ffmpeg.writeFile(inputPath, await fetchFile(file));
+
+    if (storedFormat === "webm")
+      await ffmpeg.exec(['-i', inputPath, '-fflags', '+genpts', '-preset', 'ultrafast', '-c:v', 'libvpx', '-c:a', 'libvorbis', '-crf', '23', '-threads', '0', outputPath]);
+    else
+      await ffmpeg.exec(['-i', inputPath, outputPath]);
+
+    await ffmpeg.deleteFile(inputPath);
+
+    if (splitMode) {
+      const exports = [];
+
+      const entries = await ffmpeg.listDir(tempDir);
+      for (let i = 0; i < entries.length; i++) {
+
+        const entry = entries[i];
+        if (entry.isDir) continue;
+
+        const exportPath = `${tempDir}/${entry.name}`;
+          const data = await ffmpeg.readFile(exportPath);
+        const exportBlob = new Blob([(data as Uint8Array).buffer], { type: mimeLookup[storedFormat] });
+
+        exports.push({
+          name: entry.name,
+          lastModified: new Date(),
+          input: exportBlob,
+          mode: 0o664
+        });
+
+        await ffmpeg.deleteFile(exportPath);
+      };
+
+      console.log(exports);
+
+      const blob = await downloadZip(exports).blob();
+
+      const url = URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = archiveName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      URL.revokeObjectURL(url);
+
+    }
+    else {
+      const data = await ffmpeg.readFile(outputPath);
+      const blob = new Blob([(data as Uint8Array).buffer], { type: mimeLookup[storedFormat] });
+
+      await ffmpeg.deleteFile(outputPath);
+
+      const url = URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = outputName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      URL.revokeObjectURL(url);
+    }
+
+    await ffmpeg.deleteDir(tempDir);
   });
 }
 
@@ -51,33 +138,16 @@ async function execute() {
     <div class="absolute top-2 right-2">
       <SettingsPanel />
     </div>
-    <div class="absolute top-1/7 left-1/2 transform -translate-x-1/2">
+    <div class="absolute top-1/7 min-w-96 left-1/2 transform -translate-x-1/2">
       <h1 class="text-7xl/15 lg:text-9xl/28 text-center font-semibold">Local File Converter</h1>
       <p class="text-base/10 lg:text-xl/25 text-center">"it maybe temperamental, but usually it works ok"</p>
     </div>
     <div class="absolute top-5/8 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <div class="flex flex-col lg:flex-row space-x-1 space-y-1">
         <UploadPanel @file-selected="setFile" />
-        <ConvertPanel @convert-clicked="execute" />
+        <ConvertPanel @convert-clicked="execute" @format-selected="setFormat" />
         <DownloadPanel />
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/format.ts
@@ -0,0 +1,21 @@
+
+
+export const mimeLookup : { [ext: string]: string } = {
+  'aac': 'audio/aac',
+  'apng': 'image/apng',
+  'avif': 'image/avif',
+  'avi': 'video/avi',
+  'bmp': 'image/bmp',
+  'gif': 'image/gif',
+  'jpeg': 'image/jpg',
+  'jpg': 'image/jpg',
+  'mp4': 'video/mp4',
+  'mpeg': 'video/mpeg',
+  'ogv': 'video/ogv',
+  'png': 'image/png',
+  'tif': 'image/tiff',
+  'tiff': 'image/tiff',
+  'ts': 'video/mp2t',
+  'webm': 'video/webm',
+  'webp': 'image/webp',
+};
